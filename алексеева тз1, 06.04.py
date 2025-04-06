52c6191cef04e20f28523e6cdef67d0cc78ac963
Alekseeva228 <tumonya79@gmail.com>
2025-04-06 17:26:45 +0700

    Add files via upload

---
+++ алексеева тз1, 06.04.py
@@ -0,0 +1,8 @@
+tsvet=input()
+if tsvet == "Piu Piu" or tsvet == "123":
+    print("Успешный вход")
+    print("Добро пожаловать!, Piu Piu!")
+    
+else:
+    print("Спасибо, что пользуетесь технологиями Umbrella Corporation!")
+    
